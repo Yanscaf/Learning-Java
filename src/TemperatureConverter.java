7c41afb94ce9df50dba073b939d8e121a0139e9e
Yan Miskin <yanmiskin20@gmail.com>
2025-04-01 16:17:21 +0300

    add TemperatureConverter

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введи температуру в °C: ");
+        double celsius = sc.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println(celsius + " °C = " + fahrenheit + " °F");
+    }
 }
